c78c7bedd4933d9fd74d7bc3ccafde3217189d32
Hanumant Singh <hanumant07@gmail.com>
2018-04-07 21:11:33 +0000

    utils: Cleanup kaggle setup
    Signed-off-by: Hanumant Singh <hanumant07@gmail.com>

--- utils/kaggle_setup.py
+++ utils/kaggle_setup.py
@@ -7,21 +7,14 @@ class KaggleSetup():
     def __init__(self, competition, data_path):
         if os.path.isfile(data_path):
             raise Exception('data path is a file')
-        # if not os.path.isdir(data_path):
-        #    os.makedirs(data_path)
         self.data_path = data_path
-        # self.kaggle_cmd = "kg download -u {} -p {} -c {}".format(uname, pswd, competition)
         self.kaggle_cmd = 'kaggle competitions download -c {} -p {}'.format(competition, data_path)
-        # self.kaggle_download = 'kg download -u '
 
     def download(self):
         if not os.path.isdir(self.data_path):
-            os.makedirs(data_path)
-        # cwd = os.getcwd()
-        # os.chdir(self.data_path)
-        # subprocess.run(self.kaggle_cmd, shell=True, check=True)
+            os.makedirs(self.data_path)
         subprocess.run(self.kaggle_cmd, shell=True, check=True)
-        # os.chdir(cwd)
+
     def unzip_all(self):
         for folders, subfolders, files in os.walk(self.data_path):
             for file in files:
