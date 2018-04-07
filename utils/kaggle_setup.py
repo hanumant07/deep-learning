34a85c3cfd1cf849a7c67d08522f744cb609cdb0
Hanumant Singh <hanumant07@gmail.com>
2018-04-07 15:05:59 +0000

    Improve kaggle setup script
    Signed-off-by: Hanumant Singh <hanumant07@gmail.com>

--- utils/kaggle_setup.py
+++ utils/kaggle_setup.py
@@ -1,18 +1,18 @@
-import os                                                                       
-import argparse                                                                 
-from process import subprocess                                                                                                          
+import os
+import argparse
+import subprocess
 
 parser = argparse.ArgumentParser(description='Kaggle competition setup.')
-parser.add_argument('name', metavar='N', type=str, nargs='+', 
-                     help='Provide kaggle competition name')
-NAME = parser.parse_args()                                                       
+parser.add_argument('name', type=str, help='Provide kaggle competition name')
+results = parser.parse_args()
 
-KAGGLE_CMD = -f'kaggle competitions download -c {NAME}'.format()
+competition = results.name
+KAGGLE_CMD = 'kaggle competitions download -c ' + competition
 
-FASTAI_PATH = '/home/ubuntu/src/fastai'
+FASTAI_PATH = '/home/ubuntu/fastai/fastai/'
 
-DATA_PATH = -f'/home/ubuntu/.kaggle/competetions/{NAME}'.format()                
+DATA_PATH = '/home/ubuntu/.kaggle/competitions/' + competition + '/'
 
-subprocess.run(KAGGLE_CMD, shell=True, check=True)                               
-os.symlink(FASTAI_PATH, 'fastai')                             
+subprocess.run(KAGGLE_CMD, shell=True, check=True)
+os.symlink(FASTAI_PATH, 'fastai')
 os.symlink(DATA_PATH, 'data')
